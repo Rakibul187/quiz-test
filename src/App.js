98faffcef7a659830feac7088027abc7fb119bb0
Rakibul Islam <rakibul241221@gmail.com>
2022-10-11 20:47:07 +0600

    router setup added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <h1>This is Home!!!!!</h1> },
+    { path: '/home', element: <h1>This is real Home</h1> }
+  ])
   return (
     <div className="App">
-      <h1>This is assignment 09</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
